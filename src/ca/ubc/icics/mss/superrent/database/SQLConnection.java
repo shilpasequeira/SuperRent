f015f9665a41dff67f05980a44dbd7e5c27e9337
Shilpa Sequeira <mail@shilpasequeira.com>
2015-03-31 13:42:29 -0700

    New SQL connection class that does not have connection pooling.

---
+++ src/ca/ubc/icics/mss/superrent/database/SQLConnection.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.ubc.icics.mss.superrent.database;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*; 
+import java.util.Properties;
+
+/**
+ *
+ * @author Avinash
+ */
+public class SQLConnection {
+    
+    public static Connection getConnection() {
+        Properties props = new Properties();
+        FileInputStream fis = null;
+        Connection con = null;
+        try {
+            fis = new FileInputStream("config.properties");
+            props.load(fis);
+ 
+            // load the Driver Class
+            Class.forName(props.getProperty("DB_DRIVER_CLASS"));
+ 
+            // create the connection now
+            con = DriverManager.getConnection(props.getProperty("DB_URL"),
+                    props.getProperty("DB_USERNAME"),
+                    props.getProperty("DB_PASSWORD"));
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
