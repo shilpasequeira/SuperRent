8b509ad6e79536d806354ade5eab1ba9fd6d5d97
swaij <swaij@192.168.0.84>
2015-04-05 15:20:20 -0700

    Main Login GUI, without md5 yet

--- src/ca/ubc/icics/mss/superrent/login/Login.java
+++ src/ca/ubc/icics/mss/superrent/login/Login.java
@@ -5,10 +5,37 @@
  */
 package ca.ubc.icics.mss.superrent.login;
 
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
 /**
  *
- * @author warrior
+ * @author anmolpreet sandhu
  */
-public class Login {
-    
-}
+public class Login extends Application {
+        /*
+        Image img = new Image("http://goo.gl/YsuGV");
+        ImageView view = new ImageView(img);
+        root.getChildren().add(view);
+        */
+         @Override
+    public void start(final Stage primaryStage) throws Exception {
+        
+                   
+                    Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+                    Scene login_Scene = new Scene(root, 430, 400);
+                    primaryStage.setScene(login_Scene);
+                    primaryStage.show();
+                }
+            
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+}  
+
