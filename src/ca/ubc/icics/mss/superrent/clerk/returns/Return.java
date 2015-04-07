08f8a6edaf2f077b34a71b37f5ecb6ea4f8d213f
Shilpa Sequeira <mail@shilpasequeira.com>
2015-04-07 14:03:05 -0700

    Functional Return Package files

--- src/ca/ubc/icics/mss/superrent/clerk/returns/Return.java
+++ src/ca/ubc/icics/mss/superrent/clerk/returns/Return.java
@@ -5,10 +5,298 @@
  */
 package ca.ubc.icics.mss.superrent.clerk.returns;
 
+import ca.ubc.icics.mss.superrent.clerk.rentreserve.Rent;
+import ca.ubc.icics.mss.superrent.clerk.vehiclelist.AdditionalEquipment;
+import ca.ubc.icics.mss.superrent.clerk.vehiclelist.Vehicle;
+import ca.ubc.icics.mss.superrent.database.SQLConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author warrior
  */
 public class Return {
+    final int H=3600000, D=86400000, W=604800000;
+    private int id;
+    private int rentID;
+    private Rent rentModel;
+    private Vehicle vehicleModel;
+    private int odometerReading;
+    private boolean tankFull;
+    private int grandTotal;
+    private boolean isPayWithPoints;
+    private int pointsUsed = 0;
+    private boolean isPaid;
+    private String paymentMethod;
     
+    /**
+     * Create a return object from the reserve ID
+     * 
+     * @param returnID
+     */
+    public Return (int returnID) {
+        
+        //using try-with-resources to avoid closing resources (boiler plate code)
+        try (Connection con = SQLConnection.getConnection();
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM returns WHERE "
+                        + "return_id=" + returnID);) {
+            
+            // Add the return info to the return model
+            while(rs.next()){
+                this.id = rs.getInt("reserve_id");
+                this.rentID = rs.getInt("rent_id");
+                this.odometerReading = rs.getInt("odometer_reading");
+                this.tankFull = rs.getBoolean("tank_full");
+                this.grandTotal = rs.getInt("amount");
+                this.pointsUsed = rs.getInt("points_used");
+                this.isPaid = rs.getBoolean("is_paid");
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(Return.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    
+    /**
+     * Create a return object with amount calculated.
+     * 
+     * @param rentModel rent model to use.
+     */
+    public Return (Rent rentModel) {
+        this.rentID = rentModel.getID();
+        this.rentModel = rentModel;
+        this.vehicleModel = rentModel.getVehicle();
+    }
+    
+    /**
+     * Inserts the return entry into the database.
+     * @param odometerReading
+     * @param tankFull
+     * @param paid
+     * @param paymentMethod
+     */
+    public void confirmReturn (int odometerReading, boolean tankFull, boolean paid,
+            String paymentMethod) {
+        this.odometerReading = odometerReading;
+        this.tankFull = tankFull;
+        this.isPaid = paid;
+        this.paymentMethod = paymentMethod;
+        
+        grandTotal = getGrandTotal();
+        
+        //using try-with-resources to avoid closing resources (boiler plate code)
+        try (Connection con = SQLConnection.getConnection()) {
+            try (PreparedStatement pstatement = con.prepareStatement(""
+                    + "INSERT INTO returns (rent_id, odometer_reading, "
+                    + "tank_full, amount, points_used, is_paid, payment_method) "
+                    + "values (?, ?, ?, ?, ?, ?, ?)",
+                        PreparedStatement.RETURN_GENERATED_KEYS)) {
+                System.out.println("INSERT INTO returns (rent_id, odometer_reading, "
+                    + "tank_full, amount, points_used, is_paid, payment_method)"
+                            + "values (" + rentID + "," + odometerReading + "," + 
+                        tankFull + "," + grandTotal + "," + pointsUsed + "," + paid + "," + paymentMethod + ")");
+
+                pstatement.setInt(1, rentID);
+                pstatement.setInt(2, odometerReading);
+                pstatement.setBoolean(3, tankFull);
+                pstatement.setInt(4, grandTotal);
+                pstatement.setInt(5, pointsUsed);
+                pstatement.setBoolean(6, paid);
+                pstatement.setString(7, paymentMethod);
+
+                pstatement.executeUpdate();
+
+                // If reserve was successful
+                try (ResultSet result = pstatement.getGeneratedKeys()) {
+                    while (result.next()) {
+                        this.id = result.getInt(1);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(Return.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRentID() {
+        return rentID;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+
+    public boolean isTankFull() {
+        return tankFull;
+    }
+
+    public int getGrandTotal() {
+        grandTotal = calculateGrossTotal() + calculateInsuranceCost() + calculateEquipmentTotal();
+        int newTotal = 0;
+        if (isPayWithPoints) {
+            newTotal = grandTotal - rentModel.getCustomer().getPoints();
+            pointsUsed = rentModel.getCustomer().getPoints();
+            
+            if (newTotal < 0) {
+                pointsUsed = grandTotal;
+                newTotal = 0;
+            }
+            
+            grandTotal = newTotal;
+        }
+        return grandTotal;
+    }
+
+    public int getGrossTotal() {
+        return calculateGrossTotal();
+    }
+
+    public int getInsuranceCost() {
+        return calculateInsuranceCost();
+    }
+
+    public int getEquipmentCost() {
+        return calculateEquipmentTotal();
+    }
+
+    public int getPointsUsed() {
+        return pointsUsed;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public void setIsPayWithPoints(boolean isPayWithPoints) {
+        this.isPayWithPoints = isPayWithPoints;
+    }
+    
+    /**
+     * Calculates only the cost of the vehicle over the time period
+     * @return
+     */
+    private int calculateGrossTotal() {
+        int hourlyRate = vehicleModel.getHourlyRate();
+        int dailyRate = vehicleModel.getDailyRate();
+        int weeklyRate = vehicleModel.getWeeklyRate();
+        
+        long difference = Timestamp.valueOf(LocalDateTime.now()).getTime() - 
+                rentModel.getStartDateTimestamp().getTime();
+        
+        int weeks = (int) difference / W;
+        difference %= W; 
+        int days = (int) difference / D;
+        difference %= D; 
+        int hours = (int) difference / H;
+        difference %= H;
+        
+        return weeks * weeklyRate + days * dailyRate + hours * hourlyRate;
+    }
+    
+    /**
+     * Calculates only the cost of the vehicle over the time period
+     * @return
+     */
+    private int calculateEquipmentTotal() {
+        int equipDailyRate = 0;
+        int equipHourlyRate = 0;
+        
+        ArrayList<AdditionalEquipment> additionalEquipment = rentModel.getAdditionalEquipment();
+        // If any additional equipment have been added get their rates.
+        while (additionalEquipment.iterator().hasNext()) {
+            AdditionalEquipment equipment = additionalEquipment.iterator().next();
+            equipDailyRate += equipment.getDailyRate();
+            equipHourlyRate += equipment.getHourlyRate();
+        }
+        
+        long difference = Timestamp.valueOf(LocalDateTime.now()).getTime() - 
+                rentModel.getStartDateTimestamp().getTime();
+        
+        int weeks = (int) difference / W;
+        difference %= W; 
+        int days = (int) difference / D;
+        difference %= D; 
+        int hours = (int) difference / H;
+        difference %= H;
+        
+        // Calculate the additional equipment cost.
+        return (weeks * 7 + days) * equipDailyRate +
+                hours * equipHourlyRate;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private int calculateInsuranceCost() {
+        // Get the vehicle category and type from the vehicle ID.
+        int hourlyPremium = vehicleModel.getHourlyPremium();
+        int dailyPremium = vehicleModel.getDailyPremium();
+        int weeklyPremium = vehicleModel.getWeeklyPremium();
+        
+        long difference = Timestamp.valueOf(LocalDateTime.now()).getTime() - 
+                rentModel.getStartDateTimestamp().getTime();
+        
+        int weeks = (int) difference / W;
+        difference %= W; 
+        int days = (int) difference / D;
+        difference %= D; 
+        int hours = (int) difference / H;
+        difference %= H;
+        
+        int insurance = weeks * weeklyPremium + days * dailyPremium + 
+                hours * hourlyPremium;
+        
+        // Check whether the customer is a roadstar or not.
+        if (rentModel.getCustomer().getIsRoadStar()) {
+            insurance = insurance / 2;
+        }
+        
+        return insurance;
+    }
+    
+    /**
+     * Calculates the rental time period and returns the string representation.
+     * @return 
+     */
+    public String getRentalTimePeriod() {
+        String timePeriod = "";
+        
+        long difference = Timestamp.valueOf(LocalDateTime.now()).getTime() - 
+                rentModel.getStartDateTimestamp().getTime();
+        
+        int weeks = (int) difference / W;
+        difference %= W; 
+        if (weeks > 0) {
+            timePeriod += weeks + " Week" + (weeks > 1 ? "s " : " " );
+        }
+        
+        int days = (int) difference / D;
+        difference %= D;
+        if (days > 0) {
+            timePeriod += days + " Day" + (days > 1 ? "s " : " " );
+        }
+        
+        int hours = (int) difference / H;
+        difference %= H;
+        if (hours > 0) {
+            timePeriod += hours + " Hour" + (hours > 1 ? "s " : " " );
+        }
+        
+        
+        return timePeriod;
+    }
 }
--- src/ca/ubc/icics/mss/superrent/clerk/returns/ReturnFormController.java
+++ src/ca/ubc/icics/mss/superrent/clerk/returns/ReturnFormController.java
@@ -5,9 +5,26 @@
  */
 package ca.ubc.icics.mss.superrent.clerk.returns;
 
+import ca.ubc.icics.mss.superrent.clerk.customer.Customer;
+import ca.ubc.icics.mss.superrent.clerk.rentreserve.Rent;
+import ca.ubc.icics.mss.superrent.clerk.vehiclelist.Vehicle;
+import ca.ubc.icics.mss.superrent.validation.Validate;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.print.Printer;
+import javafx.print.PrinterJob;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -15,13 +32,143 @@ import javafx.fxml.Initializable;
  * @author warrior
  */
 public class ReturnFormController implements Initializable {
-
+    private Return returnModel;
+    private static Rent rentModel;
+    private String[] PAY_METHOD = {"CASH", "CREDIT"};
+    
+    @FXML private AnchorPane anchorPane;
+    @FXML private Label errorMessageField;
+    @FXML private Label returnInvoiceNumber;
+    @FXML private Label startDateTime;
+    @FXML private Label endDateTime;
+    @FXML private Label rentalTimePeriod;
+    @FXML private Label vehicleType;
+    @FXML private Label vehiclePlateNo;
+    @FXML private Label vehicleName;
+    @FXML private Label grossTotal;
+    @FXML private Label additonalEquipmentCost;
+    @FXML private Label insuranceCost;
+    @FXML private Label grandTotal;
+    @FXML private CheckBox payWithPointsCheckbox;
+    @FXML private Label pointsUsed;
+    @FXML private TextField odometerReadingField;
+    @FXML private Label odometerReadingErrorLabel;
+    @FXML private CheckBox tankFullCheckbox;
+    @FXML private Label paymentMethodLabel;
+    @FXML private ComboBox paymentMethod;
+    @FXML private Label paymentMethodErrorLabel;
+    @FXML private Button paymentSuccessFullButton;
+    @FXML private Button printButton;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        // Get a return model with the rent ID.
+        returnModel = new Return(rentModel);
+        
+        // Initialise all fields with rental info.
+        SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+        startDateTime.setText(format.format(rentModel.getStartDateTimestamp()));
+        endDateTime.setText(format.format(new Date()));
+        rentalTimePeriod.setText(returnModel.getRentalTimePeriod());
+        
+        Vehicle vehicleModel = rentModel.getVehicle();
+        vehicleType.setText(vehicleModel.getType());
+        vehiclePlateNo.setText(vehicleModel.getPlateNumber());
+        vehicleName.setText(vehicleModel.getName());
+        
+        paymentMethod.getItems().addAll((Object[]) PAY_METHOD);
+        
+        // If customer is a club member, and has enough points, show the
+        // pay with points checkbox.
+        Customer customerModel = rentModel.getCustomer();
+        if (customerModel.getIsClubMember() && customerModel.getPoints() > 0) {
+            payWithPointsCheckbox.setVisible(true);
+            pointsUsed.setVisible(true);
+            pointsUsed.setText("You have " + customerModel.getPoints() + " points.");
+        }
+        
+        // Display the calculated amount in the text box
+        grossTotal.setText(returnModel.getGrossTotal() + " CAD");
+        additonalEquipmentCost.setText(returnModel.getEquipmentCost() + " CAD");
+        insuranceCost.setText(returnModel.getInsuranceCost() + " CAD");
+        grandTotal.setText(returnModel.getGrandTotal() + " CAD");
+    }
     
+    /**
+     * 
+     * @param rentalID
+     */
+    public static void setRentID(int rentalID) {
+        rentModel = new Rent(rentalID);
+    }
+    
+    @FXML private void payWithPointsChecked() {
+        returnModel.setIsPayWithPoints(payWithPointsCheckbox.isSelected());
+        int grandTotalCost = returnModel.getGrandTotal();
+        
+        grandTotal.setText(grandTotalCost + " CAD");
+        pointsUsed.setText(" - " + returnModel.getPointsUsed() + " points");
+        
+        if (grandTotalCost == 0) {
+            paymentMethod.setDisable(true);
+        }
+    }
+    
+    @FXML private void paymentSuccessFullButtonPressed() {
+        boolean valid = false;
+        int grandTotalCost = returnModel.getGrandTotal();
+        
+        if (grandTotalCost > 0) {
+            valid = !Validate.isEmptyTextField(odometerReadingField, odometerReadingErrorLabel) &&
+                    !Validate.isEmptyComboBox(paymentMethod, paymentMethodErrorLabel);
+        } else {
+            valid = !Validate.isEmptyTextField(odometerReadingField, odometerReadingErrorLabel);
+        }
+        
+        if (valid) {
+            errorMessageField.setVisible(false);
+            //grandTotal.setText(returnModel.getGrandTotal() + " CAD");
+            //pointsUsed.setText(returnModel.getPointsUsed() + " points used.");
+            
+            String payMethod = "";
+            if (paymentMethod.getValue() != null) {
+                payMethod = paymentMethod.getValue().toString();
+            }
+            returnModel.confirmReturn(
+                    Integer.parseInt(odometerReadingField.getText().trim()), 
+                    tankFullCheckbox.selectedProperty().getValue(), true,
+                    payMethod);
+            
+            printButton.setVisible(true);
+            paymentSuccessFullButton.setDisable(true);
+        } else {
+            errorMessageField.setText("Please check the errors.");
+        }
+    }
+    
+    @FXML private void printButtonPressed() {
+        Printer printer = Printer.getDefaultPrinter();
+        Stage dialogStage = new Stage(StageStyle.DECORATED);            
+        PrinterJob job = PrinterJob.createPrinterJob(printer);
+        if (job != null) {                    
+            boolean showDialog = job.showPageSetupDialog(dialogStage);
+            if (showDialog) {                        
+                /*anchorPane.setScaleX(0.60);
+                anchorPane.setScaleY(0.60);
+                anchorPane.setTranslateX(-220);
+                anchorPane.setTranslateY(-70);*/
+
+                boolean success = job.printPage(anchorPane);
+                if (success) {
+                    job.endJob(); 
+                } 
+                /*(anchorPane.setTranslateX(0);
+                anchorPane.setTranslateY(0);               
+                anchorPane.setScaleX(1.0);
+                anchorPane.setScaleY(1.0);*/                                       
+            }    
+        }
+    }
 }
