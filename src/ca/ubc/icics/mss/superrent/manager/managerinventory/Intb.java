5c497fae55a1e48223e1fed7f6f787cd5ce46761
dxw <dxw@dxw-pc>
2015-04-06 17:00:38 -0700

    modified size of inventory fxml

--- src/ca/ubc/icics/mss/superrent/manager/managerinventory/Intb.java
+++ src/ca/ubc/icics/mss/superrent/manager/managerinventory/Intb.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ca.ubc.icics.mss.superrent.InventoryManage;
+package ca.ubc.icics.mss.superrent.manager.managerinventory;
 
 import java.util.Date;
 import javafx.scene.image.Image;
